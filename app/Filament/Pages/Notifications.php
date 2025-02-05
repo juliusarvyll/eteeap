838e5c737fcaf9911eb53987924dcf19411952af
juliusarvyll <juliuscepeda28@gmail.com>
2025-02-05 10:01:10 +0800

    123

--- app/Filament/Pages/Notifications.php
+++ app/Filament/Pages/Notifications.php
@@ -8,6 +8,7 @@ use Filament\Notifications\Notification;
 class Notifications extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-bell';
+    protected static ?string $navigationLabel = 'Notifications';
 
     protected static string $view = 'filament.pages.notifications';
 
